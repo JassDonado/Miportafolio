23e927bd5b21b60e41385861d088e506e10eb962
JassDonado <jfdonado@unibarranquilla.edu.co>
2026-04-07 12:33:03 -0500

    foto y modificacion de textos intro y sobre mi

--- components/Body.tsx
+++ components/Body.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image"
+import Retrato from "@/imagenes/Retrato.png"
 
 export default function Body() {
     return (
@@ -5,16 +7,27 @@ export default function Body() {
         <main className="min-h-screen bg-[#0a0a0a] text-white">
         <section
           id="home"
-          className="flex min-h-screen items-center justify-center px-6 pt-28"
+          className="flex min-h-screen items-center justify-between px-6 pt-28 gap-12"
         >
-          <div className="max-w-4xl text-center">
+          <div className="max-w-2xl flex-1">
             <h1 className="text-5xl font-bold md:text-7xl">
               Jasseff <span className="text-violet-800">Dev</span>
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-300 md:text-xl">
-              Desarrollador web creando experiencias modernas, limpias y
-              funcionales.
+                Desarrollador Web & Especialista en TI
+                Creo soluciones digitales modernas, combinando
+                desarrollo web con experiencia en soporte y sistemas.
             </p>
+          </div>
+          <div className="flex-1 flex justify-center">
+            <Image 
+              src={Retrato} 
+              alt="Jasseff Dev" 
+              className="rounded-full border-4 border-violet-800 shadow-lg"
+              width={280}
+              height={250}
+              priority
+            />
           </div>
         </section>
 
@@ -22,10 +35,18 @@ export default function Body() {
           <div className="mx-auto max-w-5xl">
             <h2 className="mb-6 text-4xl font-bold">Sobre mí</h2>
             <p className="text-lg leading-8 text-gray-300">
-              Soy Jasseff, apasionado por el desarrollo web y la creación de
-              interfaces modernas. Me gusta construir proyectos con React,
-              Next.js y Tailwind CSS, cuidando tanto el diseño como la
-              experiencia del usuario.
+              Soy desarrollador web y técnico en sistemas, con experiencia
+             en soporte TI, redes y mantenimiento de infraestructura
+              tecnológica.
+
+                Actualmente trabajo en soporte N2, gestionando incidentes
+                 y asegurando la continuidad de los servicios. 
+                 Complemento esta experiencia con desarrollo web, creando
+                  soluciones digitales modernas, eficientes y enfocadas
+                   en el usuario final.
+
+                Me apasiona la tecnología, el aprendizaje continuo y la construcción
+                 de soluciones que generen impacto..
             </p>
           </div>
         </section>
