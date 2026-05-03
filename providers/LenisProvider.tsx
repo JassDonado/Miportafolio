a041ae88ef7496db4062f1de6b189424d0c9c661
JassDonado <jfdonado@unibarranquilla.edu.co>
2026-05-02 19:51:57 -0500

    act lenis2

--- providers/LenisProvider.tsx
+++ providers/LenisProvider.tsx
@@ -9,14 +9,7 @@ export default function LenisProvider({
   children: React.ReactNode;
 }) {
   useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      smooth: true,
-      mouseMultiplier: 1,
-      smoothTouch: false,
-      touchMultiplier: 2,
-    });
+    const lenis = new Lenis();
 
     function raf(time: number) {
       lenis.raf(time);
